fd1ae814403b6dbdf59d045175909928b70c46aa
Junior <35709152+git-jr@users.noreply.github.com>
2022-09-16 20:57:23 -0300

    Adicionado UI, ViewModel e LiveData

---
+++ app/src/main/java/com/paradoxo/roomwordssample/ui/activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.paradoxo.roomwordssample.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.paradoxo.roomwordssample.R
+import com.paradoxo.roomwordssample.ui.recyclerview.WordListAdapter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
